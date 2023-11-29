b0db900ddd1a55d558cec159ef7d31dc189c23c9
Lowell Alexander <performance@bidhackers.com>
2023-11-29 10:29:09 -0500

    creating repo from connection to aws

---
+++ README.md
@@ -0,0 +1,9 @@
+# FlightTracker
+This is a simple react frontend that uses a free api to find flight information about flights.
+
+Tools used:
+React
+
+React Query
+
+Tailwind
---
+++ src/CardResult.jsx
@@ -0,0 +1,30 @@
+import dateConverter from "./helpers/dateConverter";
+import { Link } from "react-router-dom";
+export default function CardResult(props) {
+  const { flight, id, reg, status } = props;
+  console.log({ props });
+
+  return (
+    <div className="card-container py-[2px] p-2">
+      <div className="bg-black/ shadow-lg  rounded-sm w-full hover:shadow-xl">
+        <Link to={`/flights/${id}`} className="w-full h-full">
+          <div className="card-header p-4 hover:p-8 transition-all duration-300">
+            <p className="title--sm text-white">
+              {dateConverter(flight.time.scheduled.departure)}
+            </p>
+            <p className="title--sm text-zinc-100">Last Update: '{status}'</p>
+          </div>
+        </Link>
+        {/* <div className="card-status-box">
+          <p className="text--lg">Status:</p>
+          <p className="text--lg">{flight.status.text}</p>
+        </div> */}
+        {/* <div className="card-cta">
+          <Link to={`/flights/${id}`} className="btn btn--block text--lg">
+          Get Flight Details
+          </Link>
+        </div> */}
+        </div>
+    </div>
+  );
+}
